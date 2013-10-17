32e160b337b6c288191d3f586393bbb00682902d
Fredrik Fahlman <ffahlman@me.com>
2013-10-17 08:33:16 +0200

    global asax change

--- Global.asax.cs
+++ Global.asax.cs
@@ -13,7 +13,7 @@ namespace AngularTutorial
     // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
     // visit http://go.microsoft.com/?LinkId=9394801
 
-    public class WebApiApplication : System.Web.HttpApplication
+    public class WebApiApplication : global::System.Web.HttpApplication
     {
         protected void Application_Start()
         {
