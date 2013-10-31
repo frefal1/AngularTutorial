a268a9955a9815f09c2841d272d007fe78468e6a
Fredrik Fahlman <ffahlman@me.com>
2013-10-31 13:46:07 +0100

    disable Seed method

--- AngularTutorial/Migrations/Configuration.cs
+++ AngularTutorial/Migrations/Configuration.cs
@@ -14,19 +14,19 @@ namespace AngularTutorial.Migrations
             AutomaticMigrationsEnabled = false;
         }
 
-        protected override void Seed(AngularTutorial.Models.AngularTutorialContext context)
-        {
-            var r = new Random();
-            var counter = 0;
-            var items = Enumerable.Range(1, 50).Select(o => new TodoItem
-            {
-                TodoItemId = o,
-                DueDate = new DateTime(2013, r.Next(1, 12), r.Next(1, 28)),
-                Priority = (byte)r.Next(10),
-                Todo = o.ToString(),
-                Added = DateTime.Today
-            }).ToArray();
-            context.TodoItems.AddOrUpdate(item => new { item.Todo }, items);
-        }
+        //protected override void Seed(AngularTutorial.Models.AngularTutorialContext context)
+        //{
+        //    var r = new Random();
+        //    var counter = 0;
+        //    var items = Enumerable.Range(1, 50).Select(o => new TodoItem
+        //    {
+        //        TodoItemId = o,
+        //        DueDate = new DateTime(2013, r.Next(1, 12), r.Next(1, 28)),
+        //        Priority = (byte)r.Next(10),
+        //        Todo = o.ToString(),
+        //        Added = DateTime.Today
+        //    }).ToArray();
+        //    context.TodoItems.AddOrUpdate(item => new { item.Todo }, items);
+        //}
     }
 }
